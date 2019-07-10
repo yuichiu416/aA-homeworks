f6225d2773ee45f61ef1f2c63e4b94c92a094de4
Roger Kiew <yuichiu416@gmail.com>
2019-07-10 18:27:45 -0500

    finished phase 5

--- W7/D3/skeleton/lib/p04_linked_list.rb
+++ W7/D3/skeleton/lib/p04_linked_list.rb
@@ -1,7 +1,7 @@
 class Node
   attr_reader :key
   attr_accessor :val, :next, :prev
-
+  
   def initialize(key = nil, val = nil)
     @key = key
     @val = val
@@ -16,11 +16,20 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    prev.next = @next
+    @next.prev = prev
   end
 end
 
 class LinkedList
+  include Enumerable
+  
   def initialize
+    @head = Node.new()
+    @tail = Node.new()
+    @head.next = @tail
+    @tail.prev = @head
+    @length = 2
   end
 
   def [](i)
@@ -29,34 +38,59 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    return nil unless include?(key)
+    each { |node| return node.val if node.key == key }
   end
 
   def include?(key)
+    each { |node| return true if node.key == key }
+    false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    @tail.prev.next = new_node
+    new_node.prev = @tail.prev
+    new_node.next = @tail
+    @tail.prev = new_node
+    new_node
   end
 
   def update(key, val)
+    each { |node| node.val = val if node.key == key }
   end
 
   def remove(key)
+    each do |node|
+      if node.key == key
+        node.prev.next = node.next
+        node.next.prev = node.prev
+      end
+    end
   end
 
-  def each
+  def each(&prc)
+    current = @head.next
+    until current == @tail
+      prc.call(current)
+      current = current.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
--- W7/D3/skeleton/lib/p05_hash_map.rb
+++ W7/D3/skeleton/lib/p05_hash_map.rb
@@ -2,6 +2,7 @@ require_relative 'p04_linked_list'
 
 class HashMap
   attr_accessor :count
+  include Enumerable
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
@@ -9,27 +10,40 @@ class HashMap
   end
 
   def include?(key)
+    @store[bucket(key)].each{ |node| return true if node.key == key }
+    false
   end
 
   def set(key, val)
+    resize! if num_buckets == @count
+    if include?(key)
+      @store[bucket(key)].update(key, val)
+    else
+      @store[bucket(key)].append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+      @store[bucket(key)].get(key)
   end
 
   def delete(key)
+    @store[bucket(key)].each{ |node| node.remove if node.key == key }
+    @count -= 1
   end
 
-  def each
+  def each(&prc)
+    @store.each { |list| list.each { |node| prc.call(node.key, node.val)} }
   end
 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -41,9 +55,14 @@ class HashMap
   end
 
   def resize!
+    store_copy = @store
+    @store = Array.new(num_buckets * 2) { LinkedList.new }
+    @count = 0
+    store_copy.each{|list| list.each { |node| set(node.key, node.val)}}
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    key.hash % num_buckets
   end
 end
